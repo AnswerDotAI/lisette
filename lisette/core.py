c9e3ec1f0e5367b1d3419f323614c449fbbdddba
Nathan Cooper <nathanacooper@proton.me>
2025-12-31 14:15:05 -0500

    add tc name in details

--- lisette/core.py
+++ lisette/core.py
@@ -508,7 +508,8 @@ def mk_tr_details(tr, tc, mx=2000):
     res = {'id':tr['tool_call_id'], 
            'call':{'function': tc.function.name, 'arguments': args},
            'result':_trunc_str(tr.get('content'), mx=mx),}
-    return f"\n\n{detls_tag}\n\n```json\n{dumps(res, indent=2)}\n```\n\n</details>\n\n"
+    summ = f"<summary>Tool call ({tc.function.name}) details</summary>"
+    return f"\n\n{detls_tag}\n{summ}\n\n```json\n{dumps(res, indent=2)}\n```\n\n</details>\n\n"
 
 # %% ../nbs/00_core.ipynb
 class AsyncStreamFormatter:
